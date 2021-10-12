35fb8ca2100c68f4b36c697e8e50168b748dca55
phbord <phbord@gmail.com>
2021-10-12 16:10:31 +0200

    first commit

---
+++ src/assets/styles/theme.js
@@ -0,0 +1,27 @@
+export default {
+  fonts: {
+    headerFont: "'Sk-Modernist', sans-serif;",
+    bodyFont: "'Poppins', sans-serif;"
+  },
+  colors: {
+    dark: "#000",
+    light: "#fff",
+  },
+  breakpoints: {
+    mobileMaxWidth: "575px",
+    tabletMinWidth: "576px",
+    desktopMinWidth: "768px"
+  },
+  width: {
+  },
+  height: {
+    headerHeight: "50px",
+  },
+  spacings: {
+    maxWidth: "970px",
+  },
+  paddings: {
+  },
+  flexs: {
+  },
+};
