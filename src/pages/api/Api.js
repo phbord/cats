3c318f499855f0cbc3e610858547d5f4aa438813
phbord <phbord@gmail.com>
2021-10-13 11:50:16 +0200

    test fetch bug

--- src/pages/api/Api.js
+++ src/pages/api/Api.js
@@ -3,21 +3,20 @@ import React, { useEffect } from 'react'
 export async function fetchAPI() {
   const config = {
     method: "GET",
-    mode: "no-cors",
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    mode: "cors",
+    // 'Access-Control-Allow-Origin': '*',
+    // 'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
     headers: {
       "Content-Type": "application/json",
+      //'Accept': 'application/json'
     },
   };
 
-  const url = 'https://latelier.co/data/cats.json';
-  //const res = await fetch(url, config).catch(err => err);
-  //const data = await res.json();
-
-  const res = fetch(url, config)
-    .then(res => res.json());
+  const url = "'Access-Control-Allow-Origin':https://latelier.co/data/cats.json";
+  const res = await fetch(url, config)
+                      .catch(err => err);
+  const data = await res.json();
 
   console.log('res: ',res);
-  //console.log('data: ',data);
+  console.log('data: ',data);
 }
